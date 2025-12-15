f8bc6d5c540f75cd53df7697da4e3c5acd418499
taco <taco@sly.nu>
2025-12-16 09:06:26 +1100

    default to ota dfu
    when booting into dfu mode, the default will ota dfu unless specifically directed to enter a different mode
    
    (cherry picked from commit db8dc4d72063f9d2117ded7d02c09a2da5a18e6f)

--- src/main.c
+++ src/main.c
@@ -253,6 +253,7 @@ static void check_dfu_mode(void) {
   bool const dfu_skip        = (gpregret == DFU_MAGIC_SKIP);
 
   bool const reason_reset_pin = (NRF_POWER->RESETREAS & POWER_RESETREAS_RESETPIN_Msk) ? true : false;
+  bool const double_reset = ((*dbl_reset_mem) == DFU_DBL_RESET_MAGIC) && reason_reset_pin;
 
   // start either serial, uf2 or ble
   bool dfu_start = _ota_dfu || serial_only_dfu || uf2_dfu ||
@@ -297,7 +298,7 @@ static void check_dfu_mode(void) {
 #endif
 
   // App mode: Double Reset detection or DFU startup for nrf52832
-  if (!(just_start_app || dfu_start || !valid_app)) {
+  if (!(just_start_app || dfu_start)) { // removed !valid_app check to allow double reset even when no app
 #ifdef NRF52832_XXAA
     /* Even DFU is not active, we still force an 1000 ms dfu serial mode when startup
      * to support auto programming from Arduino IDE
@@ -319,6 +320,10 @@ static void check_dfu_mode(void) {
     (*dbl_reset_mem) = DFU_DBL_RESET_APP;
   } else {
     (*dbl_reset_mem) = 0;
+  }
+
+  if ((dfu_start || !valid_app) && !serial_only_dfu && !uf2_dfu && !double_reset) {
+    _ota_dfu = true; // set default to OTA only when no explicit UF2/serial/dbl-reset
   }
 
   // Enter DFU mode accordingly to input
